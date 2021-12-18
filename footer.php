0f2bba744bc074da308413a7b6d2d49cfde6563f
Pavel Che <alien.killer3@bk.ru>
2021-12-18 19:59:56 +0300

    added wordpress to layout

---
+++ footer.php
@@ -0,0 +1,33 @@
+ <footer class="footer">
+    <div class="container">
+      <div class="footer__inner">
+        <a class="logo" href="#">
+          <img class="logo__img" src="images/logo.svg" alt="logo">
+        </a>
+        <div class="social footer__social">
+          <a class="social__link" href="#">
+            <img class="social__img" src="images/instagram.svg" alt="instagram icon">
+          </a>
+          <a class="social__link" href="#">
+            <img class="social__img" src="images/telegram.svg" alt="telegram icon">
+          </a>
+          <a class="social__link" href="#">
+            <img class="social__img" src="images/whatsapp.svg" alt="whatsapp icon">
+          </a>
+          <a class="social__link" href="#">
+            <img class="social__img" src="images/facebook.svg" alt="facebook icon">
+          </a>
+        </div>
+        <a class="footer__copy" href="#">
+          Политика конфиденциальности
+        </a>
+      </div>
+    </div>
+  </footer> 
+
+  <?php wp_footer(); ?>
+
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Авто из США</title>
+
+  <?php wp_head(); ?> 
+
+
+</head>
+
+<body>
+  <header class="header">
+    <div class="container">
+      <div class="header__top">
+        <a class="logo" href="#">
+          <img class="logo__img" src="images/logo.svg" alt="logo">
+        </a>
+        <a class="phone" href="tel:+380505556677">
+          +38 (050) 555 66 77
+        </a>
+      </div>
+      <div class="header__content">
+        <h1 class="header__title">авто из сша “под ключ”</h1>
+        <h2 class="header__subtitle">
+          ЗАКАЖИ СЕБЕ АВТО ИЗ США С ВЫГОДОЙ ДО 40%
+        </h2>
+        <p class="header__text">
+          Подбор, покупка, доставка, растаможка, ремонт, оформление по договору
+        </p>
+        <a class="button" href="#">КОНСУЛЬТАЦИЯ ЭКСПЕРТА</a>
+        <div class="social header__social">
+          <a class="social__link" href="#">
+            <img class="social__img" src="images/instagram.svg" alt="instagram icon">
+          </a>
+          <a class="social__link" href="#">
+            <img class="social__img" src="images/telegram.svg" alt="telegram icon">
+          </a>
+          <a class="social__link" href="#">
+            <img class="social__img" src="images/whatsapp.svg" alt="whatsapp icon">
+          </a>
+          <a class="social__link" href="#">
+            <img class="social__img" src="images/facebook.svg" alt="facebook icon">
+          </a>
+        </div>
+        <img class="header__images" src="images/mustang.png" alt="ford mustang">
+      </div>
+    </div>
+  </header>
